f18d5ef7986c876a907b39268b6451d4deef660b
rodriguezje96 <rodriguez.je96@gmail.com>
2022-05-18 13:24:06 -0300

    Primer desafío

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         José Rodríguez. Primer desafío de React.
         </p>
         <a
           className="App-link"
