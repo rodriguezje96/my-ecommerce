d2449dddb3c29e75535c00959a18009f80ae4ea3
rodriguezje96 <rodriguez.je96@gmail.com>
2022-05-22 17:09:50 -0300

    Navbar
    -Se agrega navbar con links
    -Se modifica un poco el css
    -Se instala Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/NavBar.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Navbar></Navbar>
+        <br />
         <p>
-         José Rodríguez. Primer desafío de React.
+          José Rodríguez. Segundo desafío de React.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
