1107ffb262b8abd6bd9481bea7bdeeafa9f590f5
rodriguezje96 <rodriguez.je96@gmail.com>
2022-06-07 19:48:54 -0300

    Primera Entrega tp final

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,19 +1,11 @@
-import { useNavigate } from "react-router-dom"
 import { Counter } from '../components/ItemCount';
 
 
 export const ItemDetail = ({ item }) => {
 
-    const navigate = useNavigate();
-
-    const handleVolver = () => {
-        navigate(-1);
-    }
-
 
     return (
         <div>
-            <button className="btn btn-dark" onclick={handleVolver}>Volver</button>
             <div className="container">
                 <div className="card container">
                     <img className="card-img-top" src={item?.img} alt={item?.nombre}></img>
