dfec5eb9ad3e92c532c49d90edcdee0e6cc144ae
José Rodríguez <84817384+rodriguezje96@users.noreply.github.com>
2022-07-11 16:12:15 -0300

    Desafío Final

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -3,41 +3,30 @@ import { CartContext } from './CartContext.js'
 import { Navigate } from "react-router-dom";
 import { collection, addDoc } from "firebase/firestore"
 import { db } from '../firebase/config.js';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const schema = Yup.object({
+    nombre: Yup.string()
+             .required('Este campo es obligatorio')
+             .min(4, 'El nombre es demasiado corto')
+             .max(30, 'Maximo 30 caracteres'),
+    email: Yup.string()
+            .required('Este campo es obligatorio')
+            .email('No parece un mail real'),
+    direccion: Yup.string()
+            .required('Este campo es obligatorio')
+            .min(4, 'La direccion es demasiado corta')
+            .max(30, 'Maximo 30 caracteres')       
+}).shape
 
 export const Checkout = () => {
     const { cart, totalPrice, emptyCart } = useContext(CartContext);
 
     const [orderId, setOrderId] = useState(null)
-    const [values, setValues] = useState({
-        nombre: '',
-        email: '',
-        telefono: ''
-    })
- 
 
-    const handleInputChange = (e) => {
-        setValues({
-            ...values,
-            [e.target.name]: e.target.value
-        })
-    }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        if (values.nombre.length < 5) {
-            alert("Ingresa tu nombre completo")
-            return
-        }
-        if (values.email.length < 5) {
-            alert("El email es invalido")
-            return
-        }
-        if (values.telefono.length < 5) {
-            alert("Parece que faltan numeros")
-            return
-        }
-
+    const createOrder = async (values) => {
 
         const orden = {
             buyer: values,
@@ -72,34 +61,53 @@ export const Checkout = () => {
 
             <h2>Checkout</h2>
 
-            <form onSubmit={handleSubmit}>
-                <input
-                    value={values.nombre}
-                    name="nombre"
-                    onChange={handleInputChange}
-                    type={"text"}
-                    placeholder="Juan Perez"
-                    className="form-control my-2">
-                </input>
-                <input
-                    value={values.email}
-                    name="email"
-                    onChange={handleInputChange}
-                    type={"email"}
-                    placeholder="tumail@gmail.com"
-                    className="form-control my-2">
-                </input>
-                <input
-                    value={values.telefono}
-                    name="telefono"
-                    onChange={handleInputChange}
-                    type={"number"}
-                    placeholder="1122334455"
-                    className="form-control my-2">
-                </input>
+            <Formik initialValues={{
+                nombre: "",
+                email: "",
+                direccion: ""
+            }}
+                onSubmit={createOrder}
+                validationSchema={schema}
+            >
+                {(formik) => (
+                    <form onSubmit={formik.handleSubmit}>
+                        <input
+                            value={formik.values.nombre}
+                            name="nombre"
+                            onChange={formik.handleChange}
+                            type={"text"}
+                            placeholder="Juan Perez"
+                            className="form-control my-2">
+                        </input>
+                        {formik.errors.nombre && <p className="alert alert-danger">{formik.errors.nombre}</p>}
 
-                <button onClick={handleSubmit} type="submit" className="btn btn-dark my-2">Enviar</button>
-            </form>
+                        <input
+                            value={formik.values.email}
+                            name="email"
+                            onChange={formik.handleChange}
+                            type={"email"}
+                            placeholder="tumail@gmail.com"
+                            className="form-control my-2">
+                        </input>
+                        {formik.errors.email && <p className="alert alert-danger">{formik.errors.email}</p>}
+
+                        <input
+                            value={formik.values.telefono}
+                            name="telefono"
+                            onChange={formik.handleChange}
+                            type={"number"}
+                            placeholder="1122334455"
+                            className="form-control my-2">
+                        </input>
+                        {formik.errors.telefono && <p className="alert alert-danger">{formik.errors.telefono}</p>}
+
+
+                        <button onClick={handleSubmit} type="submit" className="btn btn-dark my-2">Enviar</button>
+                    </form>
+                )}
+            </Formik>
+
+
             <button onClick={emptyCart} className="btn btn-danger">Cancelar Compra</button>
 
         </div>
