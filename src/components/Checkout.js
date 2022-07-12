344bc92e8a381afc47c325bcef8fdb9f716ffdb9
rodriguezje96 <rodriguez.je96@gmail.com>
2022-07-11 23:46:17 -0300

    Desafio Final

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -3,102 +3,105 @@ import { CartContext } from './CartContext.js'
 import { Navigate } from "react-router-dom";
 import { collection, addDoc } from "firebase/firestore"
 import { db } from '../firebase/config.js';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-
-const schema = Yup.object({
-    nombre: Yup.string()
-             .required('Este campo es obligatorio')
-             .min(4, 'El nombre es demasiado corto')
-             .max(30, 'Maximo 30 caracteres'),
-    email: Yup.string()
-            .required('Este campo es obligatorio')
-            .email('No parece un mail real'),
-    direccion: Yup.string()
-            .required('Este campo es obligatorio')
-            .min(4, 'La direccion es demasiado corta')
-            .max(30, 'Maximo 30 caracteres')       
-}).shape
-
 
 export const Checkout = () => {
     const { cart, totalPrice, emptyCart } = useContext(CartContext);
 
     const [orderId, setOrderId] = useState(null)
-
     const [values, setValues] = useState({
         nombre: '',
         email: '',
         telefono: ''
     })
+ 
 
+    const handleInputChange = (e) => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
+    }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-
-    const createOrder = async (values) => {
-
-            const orden = {
-                buyer: values,
-                items: cart?.map(({ id, cantidad, nombre, precio }) => ({ id, cantidad, nombre, precio })),
-                total: totalPrice()
-            }
-            const ordersRef = collection(db, "orders")
-            addDoc(ordersRef, orden)
-                .then((doc) => {
-                    setOrderId(doc.id)
-                    emptyCart()
-
-                })
+        if (values.nombre.length < 5) {
+            alert("Ingresa tu nombre completo")
+            return
+        }
+        if (values.email.length < 5) {
+            alert("El email es invalido")
+            return
+        }
+        if (values.telefono.length < 5) {
+            alert("Parece que faltan numeros")
+            return
         }
 
-        if (orderId) {
-            return (
-                <div>
-                    <h2>Gracias por su compra!</h2>
-                    <p>Su código de orden es: {orderId}</p>
-                </div>
-            )
-        }
 
-        if (cart?.length === 0) {
-            return (
-                <Navigate to="./"></Navigate>
-            )
+        const orden = {
+            buyer: values,
+            items: cart?.map(({ id, cantidad, nombre, precio }) => ({ id, cantidad, nombre, precio })),
+            total: totalPrice()
         }
+        const ordersRef = collection(db, "orders")
+        addDoc(ordersRef, orden)
+            .then((doc) => {
+                setOrderId(doc.id)
+                emptyCart()
+
+            })
+    }
+
+    if (orderId) {
         return (
-            <div className="container">
-
-                <h2>Checkout</h2>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        value={values.nombre}
-                        name="nombre"
-                        onChange={handleInputChange}
-                        type={"text"}
-                        placeholder="Juan Perez"
-                        className="form-control my-2">
-                    </input>
-                    <input
-                        value={values.email}
-                        name="email"
-                        onChange={handleInputChange}
-                        type={"email"}
-                        placeholder="tumail@gmail.com"
-                        className="form-control my-2">
-                    </input>
-                    <input
-                        value={values.telefono}
-                        name="telefono"
-                        onChange={handleInputChange}
-                        type={"number"}
-                        placeholder="1122334455"
-                        className="form-control my-2">
-                    </input>
-
-                    <button onClick={handleSubmit} type="submit" className="btn btn-dark my-2">Enviar</button>
-                </form>
-                <button onClick={emptyCart} className="btn btn-danger">Cancelar Compra</button>
-
+            <div>
+                <h2>Gracias por su compra!</h2>
+                <p>Su código de orden es: {orderId}</p>
             </div>
         )
     }
+
+    if (cart?.length === 0) {
+        return (
+            <Navigate to="./"></Navigate>
+        )
+    }
+    return (
+        <div className="container">
+
+            <h2>Checkout</h2>
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    value={values.nombre}
+                    name="nombre"
+                    onChange={handleInputChange}
+                    type={"text"}
+                    placeholder="Juan Perez"
+                    className="form-control my-2">
+                </input>
+                <input
+                    value={values.email}
+                    name="email"
+                    onChange={handleInputChange}
+                    type={"email"}
+                    placeholder="tumail@gmail.com"
+                    className="form-control my-2">
+                </input>
+                <input
+                    value={values.telefono}
+                    name="telefono"
+                    onChange={handleInputChange}
+                    type={"number"}
+                    placeholder="1122334455"
+                    className="form-control my-2">
+                </input>
+
+                <button onClick={handleSubmit} type="submit" className="btn btn-dark my-2">Enviar</button>
+            </form>
+            <button onClick={emptyCart} className="btn btn-danger">Cancelar Compra</button>
+
+        </div>
+    )
+}
